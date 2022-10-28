f50c9ff7c9999598089abd7892c2763c7bce9dc1
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2022-10-28 01:42:59 -0300

    if js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do veiculo é de ${vel}` )
+if(vel > 60){
+    console.log(`voce ultrapassou a velocidade permitida, voce foi multado`)
+}
+console.log(`Digira sempre usando cinto de segurança`)
