410995cf0df5255630b1708594caf0fd8ecd43e3
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2022-11-01 01:17:18 -0300

    primeiro dessafio js

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,26 @@
+function carregar (){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //hora = data.getHours()    
+    hora = 19
+    minutos = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos `
+    if (hora >= 0 && hora < 12 ){
+        //bom dia
+        img.src = 'imagens/manha.jpg'
+        document.body.style.background = '#B4814A'
+
+    }else if (hora >= 12 && hora < 18){
+        //boa tarde
+        img.src = 'imagens/tarde.jpg'
+        document.body.style.background = '#8599B2'
+        
+    }else{
+        // boa noite 
+        img.src = 'imagens/noite.jpg'
+        document.body.style.background = '#1F4755'
+    }
+        
+}
+
