f1d15ecb6e5be43ad418504793c0d1421a472932
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2022-11-03 00:08:58 -0300

    imagens para o proximo desafio

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,8 +2,7 @@ function carregar (){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //hora = data.getHours()    
-    hora = 19
+    hora = data.getHours()        
     minutos = data.getMinutes()
     msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos `
     if (hora >= 0 && hora < 12 ){
@@ -14,12 +13,13 @@ function carregar (){
     }else if (hora >= 12 && hora < 18){
         //boa tarde
         img.src = 'imagens/tarde.jpg'
-        document.body.style.background = '#8599B2'
-        
+        document.body.style.background = '#8599B2'    
+
     }else{
         // boa noite 
         img.src = 'imagens/noite.jpg'
         document.body.style.background = '#1F4755'
+        
     }
         
 }
